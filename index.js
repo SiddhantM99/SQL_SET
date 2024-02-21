8f9bd70c995fadcdefd6bcae12c4cd086d9be226
Siddhant mishra <siddhantmishra@Siddhants-MacBook-Air.local>
2024-02-21 11:10:27 +0530

    CRUD operation on SQL database

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2'); 
 
 const express = require('express');
-
-
 const app = express();
+
+const path = require('path');
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+let getUser = () => {
+  return [
+    faker.datatype.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+
+
 const connection =  mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -63,21 +82,130 @@ app.listen(8080, (req,res) => {
   console.log(`port is listening `);
 });
 
-app.use("/", (req,res) => {
-
+app.get("/", (req,res) => {
   let q =`SELECT count(*) FROM user`;
   try{
       connection.query(q, (err, result) => {
           if(err) throw err;
-          console.log(result[0]["count(*)"]);
-          res.send("success");
-         
+         let count = result[0]["count(*)"];
+          res.render("home.ejs", {count});
 
         });    
     } catch(err) {
       console.log(err);
       res.send("Some error in DB");
     }
+});
+
+
+
+app.get("/user", (req,res) => {
+
+  let q =`SELECT * FROM user`;
+  try{
+      connection.query(q, (err, users) => {
+          if(err) throw err;
+        res.render("showuser.ejs", {users});
+        });    
+    } catch(err) {
+      console.log(err);
+      res.send("Some error in DB");
+    }
+});
+
+// EDIT PAGE (DB)
+app.get("/user/:id/edit", (req, res) => {
+  let {id} = req.params;
+ let q = `SELECT * FROM user WHERE id='${id}'`;
+ try{
+   connection.query(q, (err, result) => {
+       if(err) throw err;
+       let user = result[0];
+       res.render("edit.ejs", {user});
+     });    
+ } catch(err) {
+   console.log(err);
+   res.send("Some error in DB");
+ }
+});
+
+// UPDATE ROUTE (DB)
+
+app.patch("/user/:id", (req, res) => {
+  let {id} = req.params;
+  let {password: formPass, username: newUsername} = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try{
+    connection.query(q, (err, result) => {
+        if(err) throw err;
+        let user = result[0];
+        if(formPass != user.password)
+        {
+          res.send("Wrong password");
+        }
+
+        else{
+          let  q2 = `UPDATE user SET Username='${newUsername}' WHERE id='${id}'`;
+          connection.query(q2, (err, result) => {
+            if (err) throw err;
+            res.redirect("/user");
+          });
+        }
+      });    
+  } catch(err) {
+    console.log(err);
+    res.send("Some error in DB");
+  }
+});
+
+app.get("/user/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/user/new", (req, res) => {
+  let { username, email, password } = req.body;
+  let id = uuidv4();
+  //Query to Insert New User
+  let q = `INSERT INTO user (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log("added new user");
+      res.redirect("/user");
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
+});
+
+app.delete("/user/:id/", (req, res) => {
+  let { id } = req.params;
+  let { password } = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+
+      if (user.password != password) {
+        res.send("WRONG Password entered!");
+      } else {
+        let q2 = `DELETE FROM user WHERE id='${id}'`; //Query to Delete
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          else {
+            console.log(result);
+            console.log("deleted!");
+            res.redirect("/user");
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
 });
 
 
